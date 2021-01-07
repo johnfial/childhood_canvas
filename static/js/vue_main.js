90518bd771e4dc8046a8ca9966d03ae03b7d7ed7
johnfial <12399190+johnfial@users.noreply.github.com>
2021-01-06 16:16:56 -0800

    lookOK, MVP, +draw ability, only A and C do anything

--- static/js/vue_main.js
+++ static/js/vue_main.js
@@ -11,35 +11,48 @@ new Vue({
     data: {
         vueCanvas: null,
         is_drawing: false,
-        key_lookups: {
-        },
         x: null,
         y: null,
+        keys_and_functions: {
+            Space: `console.log('this')`,
+            KeyA: console.log('Im a function inside a value, inside an object, inside the data object, isnide the VUE shell!!!!'),
+            KeyA: `test`,
+        },
         example_array: [],
         example_string: "",
         example_integer: 1,
         example_boolean: false,
     },
     methods: {
-        logKey: function(event) {
+        logKey: function(KeyboardEvent) {
+            // NOTES:
+            // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code
+            // KeyboardEvent.code is the PHYSICAL key pressed (regardless of modifier OR keyboard layout) 
+            // while KeyboardEvent.key is the 'sent' key...
+            // while KeyboardEvent.keyCode is the numerical code...
+
             // basic right-side keyboard logs:
-            console.log(`event.code: ${event.code}.`)
-            keyboard_logs.textContent += ` ${event.key} ` 
+            console.log(`logKey: function(KeyboardEvent) running with KeyboardEvent.code: ${KeyboardEvent.code} and KeyboardEvent.key : ${KeyboardEvent.key}.`)
+            keyboard_logs.textContent += ` ${KeyboardEvent.code}`
+            last_keypress.textContent = KeyboardEvent.code
+
+            console.log('here follows keys_and_functions test : this.keys_and_functions.key_lookup')
+            let key_lookup = KeyboardEvent.code // change this to experiment
+            console.log(this.keys_and_functions[key_lookup])
         
             // Working keypresses:
-            if (event.code === 'Space') {
+            if (KeyboardEvent.code === 'KeyC') {
                 this.draw_face(50)
             }
-            if (event.keyCode == 91) { // 91 is the ⊞ WINDOWS key
+            if (KeyboardEvent.keyCode == 91) { // 91 is the ⊞ WINDOWS key
                 // event.preventDefault(); // won't work
                 console.log("Win Key was clicked.")
                 this.draw_gradient()
             }
-            if (event.code === 'Alt') {
-                // event.preventDefault()
+            if (KeyboardEvent.code === 'Alt') {
                 console.log("Alt Key was clicked.")
             }
-            if (event.code === 'KeyA' ) {
+            if (KeyboardEvent.code === 'KeyA' ) {
                 // generate_random_x_y(window.innerWidth, window.innerHeight)
                 console.log(`Window size: ${window.innerWidth} * ${window.innerHeight}.`)
                 this.draw_circle_random(window.innerWidth, window.innerHeight)
@@ -47,13 +60,14 @@ new Vue({
 
             // Section 2:
             // Here I'm attempting to make any digit (which are inconveniently 'string' of 'Digit3' or 'Numpad3'...) draw the same thing...
-                // console.log(typeof(event.code))
+                // console.log(typeof(KeyboardEvent.code)) // string
         },
-        log_this: function() {
+        log_this: function(e) {
             // unused
-            console.log(`logging log_this`)            
-        },        
-        draw_circle_random: function(input_x, input_y) {    
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        draw_circle_random: function(input_x, input_y) {
             let canvas = this.vueCanvas
             console.log('draw_circle_random() function.')
 
@@ -147,10 +161,12 @@ new Vue({
             canvas.closePath()
         },
         draw: function(e) { // thanks to: https://dev.to/reiallenramos/drawing-in-vue-using-mousemove-event-34cg
+            // this.x = e.offsetX // don't confuse with clientX 
+            // this.y = e.offsetY // and clientY, which are absolute
             if(this.is_drawing) {
             this.draw_line(this.x, this.y, e.offsetX, e.offsetY)
-            this.x = e.offsetX // don't confuse with clientX 
-            this.y = e.offsetY // and clientY, which are absolute
+            this.x = e.offsetX 
+            this.y = e.offsetY
             }
         },
         start_drawing: function(e) {
@@ -171,6 +187,8 @@ new Vue({
         console.log('mounted:function()')
         var canvas = document.getElementById('canvas')
         this.vueCanvas = canvas.getContext('2d')
+        // const width = 800
+        // const height = 800
 
         document.addEventListener('keypress', this.logKey) // huge difference between keyup and keydown!
     },
